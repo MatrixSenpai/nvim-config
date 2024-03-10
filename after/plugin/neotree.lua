53aa5d9a9039ab0e40d3f90323e0e8b9d342eac4
MatrixSenpai <math.matrix@icloud.com>
2024-03-10 04:47:17 -0500

    some small stuffs

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,8 +1,10 @@
 local neotree = require('neo-tree')
 neotree.setup({
     filesystem = {
+        visible = true,
         hijack_netrw_behavior = 'open_current'
     }
 })
 
 vim.keymap.set('n', '<leader>pv', '<cmd>Neotree current<CR>')
+vim.keymap.set('n', '<leader>pl', '<cmd>Neotree<CR>')
